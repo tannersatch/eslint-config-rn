b2557a2dfdcd11b5349ff5ec290d9e9db7464fb6
Tanner Satchwell <tsatchwell@fileshadow.com>
2021-03-24 21:49:28 -0600

    back to 80 width

--- eslintrc.js
+++ eslintrc.js
@@ -99,7 +99,7 @@ module.exports = {
       {
         trailingComma: 'es5',
         singleQuote: true,
-        printWidth: 120,
+        printWidth: 80,
         //below line only for window users facing CLRF and eslint/prettier error
         // non window users feel free to delete it
         endOfLine: 'auto'
